f108935c2c4a6f2180cf3fe598e08831d0ec2f07
zsakvo <gdwqsp@gmail.com>
2021-08-25 00:44:37 +0800

    自动初始化数据库

--- src/main/events/cirnoIpcs.ts
+++ src/main/events/cirnoIpcs.ts
@@ -1,10 +1,12 @@
 import { ipcMain, IpcMainEvent } from 'electron';
+import db from '../plugins/datastore';
 
 export default {
   listen() {
     console.log('开始监听 IPC 事件');
     ipcMain.on('setShelfList', async (evt: IpcMainEvent, shelfList) => {
       console.log(shelfList);
+      console.log(db);
     });
   }
 };
--- src/main/plugins/datastore.ts
+++ src/main/plugins/datastore.ts
@@ -2,10 +2,21 @@ import { app } from 'electron';
 import Datastore from 'lowdb';
 import FileSync from 'lowdb/adapters/FileSync';
 import path from 'path';
+import fs from 'fs-extra';
 
 const STORE_PATH = app.getPath('userData');
-console.log(STORE_PATH);
-const adapter = new FileSync(path.join(STORE_PATH, '/data.json'));
-const db = Datastore(adapter);
 
-export default db;
+if (!fs.pathExistsSync(path.join(STORE_PATH, 'db'))) {
+  fs.mkdirpSync(path.join(STORE_PATH, 'db'));
+}
+
+console.log(STORE_PATH);
+const shelfDB = Datastore(new FileSync(path.join(STORE_PATH, 'db', 'shelf.json')));
+const bookDB = Datastore(new FileSync(path.join(STORE_PATH, 'db', 'book.json')));
+const cptDB = Datastore(new FileSync(path.join(STORE_PATH, 'db', 'cpt.json')));
+
+export default {
+  shelfDB,
+  bookDB,
+  cptDB
+};
