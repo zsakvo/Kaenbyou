3c90e0c179e68c53b312e0ff5dc811377a8f34b2
zsakvo <gdwqsp@gmail.com>
2021-09-01 21:07:57 +0800

    vuex 修正

--- src/render/store/index.ts
+++ src/render/store/index.ts
@@ -1,20 +1,26 @@
-// import { state } from './state';
-// import { createStore } from 'vuex';
-// import { SHELF } from './const';
-// import { getShelfBookList, getShelfList } from '@/api';
+import { state } from './state';
+import { createStore } from 'vuex';
+import { SHELF } from './const';
+import { getShelfList } from '@/api';
 
-// export default createStore({
-//   state,
-//   mutations: {
-//     [SHELF]:(state,data)=>{
-
-//     }
-//   },
-//   actions: {
-//     getShelfs:async()=>{
-//       const res = await getShelfList();
-//       state.shelfList = res.shelf_list;
-//     }
-//   },
-//   modules: {}
-// });
+export default createStore({
+  state,
+  getters: {
+    shelfList: (state) => {
+      state.shelf.list;
+    }
+  },
+  mutations: {
+    [SHELF]: (state, data) => {
+      state.shelf = data;
+    }
+  },
+  actions: {
+    getShelfs: async ({ commit, state }) => {
+      const res = await getShelfList();
+      state[SHELF].list = res.shelf_list;
+      commit('shelf/list', res.shelf_list);
+    }
+  },
+  modules: {}
+});
