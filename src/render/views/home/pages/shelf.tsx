a0e1423adc18f0066bda0d2a850b3dd47619016b
zsakvo <gdwqsp@gmail.com>
2021-09-07 22:39:47 +0800

    支持 下拉刷新

--- src/render/views/home/pages/shelf.tsx
+++ src/render/views/home/pages/shelf.tsx
@@ -52,6 +52,53 @@ export default defineComponent({
       await getShelfList();
       state.loading = false;
     };
+    const PHASE = {
+      moving: {
+        enter: 'enter',
+        leave: 'leave'
+      },
+      fetching: 'fetching',
+      succeed: 'succeed'
+    };
+    const tipText = ref('');
+    const pullingDownHandler = async () => {
+      setTipText(PHASE.fetching);
+      await onRefresh();
+      await nextTick();
+      setTipText(PHASE.succeed);
+      scroll.finishPullDown();
+    };
+    const setTipText = (phase) => {
+      console.log('phase', phase);
+      const TEXTS_MAP = {
+        enter: '下拉刷新',
+        leave: '松手刷新',
+        fetching: '正在读取数据',
+        succeed: '刷新成功'
+      };
+      tipText.value = TEXTS_MAP[phase];
+    };
+    const initScroll = () => {
+      scroll = new BScroll(scrollWrapper.value as any, {
+        scrollY: true,
+        scrollbar: true,
+        pullDownRefresh: {
+          threshold: 90,
+          stop: 45
+        }
+      });
+      scroll.on('pullingDown', pullingDownHandler);
+      scroll.on('scrollEnd', (e) => {
+        console.log('scrollEnd', e);
+      });
+      // v2.4.0 supported
+      scroll.on('enterThreshold', () => {
+        setTipText(PHASE.moving.enter);
+      });
+      scroll.on('leaveThreshold', () => {
+        setTipText(PHASE.moving.leave);
+      });
+    };
     const fetchBooks = async () => {
       const res = await getShelfBookList(state.shelfId, state.page);
       console.log(res);
@@ -63,11 +110,7 @@ export default defineComponent({
       await nextTick();
       console.log('books--->', scrollWrapper.value);
       // const wrapper = document.querySelector('._pull-wrapper_1nvvv_29');
-      scroll = new BScroll(scrollWrapper.value as any, {
-        scrollY: true,
-        scrollbar: true,
-        pullDownRefresh: true
-      });
+      initScroll();
     };
 
     const toReader = (book: any) => {
@@ -84,7 +127,7 @@ export default defineComponent({
         }
       });
     };
-    return { icons, state, onRefresh, toReader, scrollWrapper, refresh, scroll };
+    return { icons, state, onRefresh, toReader, scrollWrapper, refresh, scroll, tipText };
   },
   render() {
     return (
@@ -104,19 +147,24 @@ export default defineComponent({
             ref="refresh"
           > */}
           <div class={styles.pullWrapper} ref="scrollWrapper">
-            <div class={styles.books}>
-              {this.state.shelfBooks.map((book: any, index: number) => (
-                <div class={styles.bookCard} key={index} onClick={() => this.toReader(book)}>
-                  <div class={styles.cover}>
-                    <img src={book.book_info.cover} alt="" />
+            <div class="pulldown-scroller">
+              <div class={styles.pulldownWrapper}>
+                <div>{this.tipText}</div>
+              </div>
+              <div class={styles.books}>
+                {this.state.shelfBooks.map((book: any, index: number) => (
+                  <div class={styles.bookCard} key={index} onClick={() => this.toReader(book)}>
+                    <div class={styles.cover}>
+                      <img src={book.book_info.cover} alt="" />
+                    </div>
+                    <div class={styles.info}>
+                      <div class={styles.name}>{book.book_info.book_name}</div>
+                      <div class={styles.author}>{book.book_info.author_name}</div>
+                      <div class={styles.new}>{book.book_info.last_chapter_info.chapter_title}</div>
+                    </div>
                   </div>
-                  <div class={styles.info}>
-                    <div class={styles.name}>{book.book_info.book_name}</div>
-                    <div class={styles.author}>{book.book_info.author_name}</div>
-                    <div class={styles.new}>{book.book_info.last_chapter_info.chapter_title}</div>
-                  </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
           </div>
 
