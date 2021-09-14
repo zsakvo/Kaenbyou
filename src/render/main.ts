19e06e4c94f7044f9a99be8894a532d44f0c7ade
zsakvo <gdwqsp@gmail.com>
2021-09-14 17:54:27 +0800

    移除 vue3-perfect-scrollbar

--- src/render/main.ts
+++ src/render/main.ts
@@ -4,10 +4,8 @@ import router from './router';
 import store from './store';
 import '@/style/index.scss';
 import '@vant/touch-emulator';
-import PerfectScrollbar from 'vue3-perfect-scrollbar';
-import 'vue3-perfect-scrollbar/dist/vue3-perfect-scrollbar.css';
 import { ConfigProvider } from 'vant';
 
 window.$store = store;
 
-createApp(App).use(router).use(store).use(ConfigProvider).use(PerfectScrollbar).mount('#app');
+createApp(App).use(router).use(store).use(ConfigProvider).mount('#app');
